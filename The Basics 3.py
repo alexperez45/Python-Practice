ac3a0b64cf29a8e5d4f991a9f91cde91a4abaedd
alexperez45 <alperez296@gmail.com>
2024-04-18 14:30:58 -0400

    added weight conversion program

--- The Basics 3.py
+++ The Basics 3.py
@@ -1,1 +1,13 @@
 #Here I will expirment with if statments, loops, lists and more
+weight = int(input ("How much do you weigh? "))
+metric = input ("(K)g or (L)bs: ")
+if metric.upper() == 'K':
+    weight = weight * 2.205
+    weight = round(weight,2)
+    print ('Weight in Lbs: ' + str(weight))
+elif metric.upper() == 'L':
+    weight = weight / 2.205
+    weight = round(weight,2)
+    print ("Weight in Kg: " + str(weight))
+else:
+    print ("Please try again")
