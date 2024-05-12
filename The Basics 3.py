683e62320b20f558f019bb23056a3022d4c6e415
alexperez45 <alperez296@gmail.com>
2024-05-12 10:52:04 -0400

    Added a for loop

--- The Basics 3.py
+++ The Basics 3.py
@@ -1,4 +1,4 @@
-#Here I will expirment with if statments, loops, lists and more
+#Here I will expirment with if statments, loops, and more
 import random
 import time
 weight = input ("How much do you weigh? ")
@@ -28,32 +28,10 @@ while x > 1:
     print (x * '*')
     x = x - 1
 
-books = ['Frankenstein', 'David Copperfield', 'Brave New World', 'Moby Dick', 'Little Woman']
-authors = ['Shelley', 'Dickens', 'Huxley', 'Melville', 'Alcott']
-
-for x in range(len(books)):
-    input ('Who wrote ' + books[x] + '? ')
-
-print ('\nThe correct book titles and authors are:')
-
-for x in range(len(authors)):
-    print (books[x] + ' - ' + authors[x])
-
-print ('Little Woman' in  books)
-books.clear ()
-authCount = authors.count("Shelley")
-print (authCount)
-print (books)
-print (authors)
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
- 
-two_lists = [fruits, vegetables]
-number2 = random.randint(0,4)
-number1 = random.randint(0,6)
- 
-print(two_lists[0][2])
-print(two_lists[1][2])
-print(two_lists[0][number1])
-print(two_lists[1][number2])
+#Calculates the sum of all the even numbers from 1 to X.
+target = int(input("Type a number: "))
+target += 1
+total = 0
+for number in range(0, target, 2):
+  total += number
+print (total)
